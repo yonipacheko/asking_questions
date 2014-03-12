5bf4edafd9479f37a74479b7abcada778bba97b3
yoni pacheko <fernando.lbarrajon8@gmail.com>
2014-03-12 16:10:45 +0100

    wip

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
 
 
-  validates :user, uniqueness: {scope: current_user, message: "User can only vote on this answer one time" }
+  validates :user, uniqueness: {scope: :user, message: "User can only vote on this answer one time" }
 end

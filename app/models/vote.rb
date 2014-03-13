14dd845154d6bb930c94fba828a539899abe5a24
moralesalberto <alberto@moralitos.com>
2014-03-13 08:03:55 -0400

    adding the vote testing

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,17 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
 
+  validate :only_one_vote
 
-  validates :user, uniqueness: {scope: :user_id, message: "User can only vote on this answer one time" }
+
+  def only_one_vote
+    count = Vote.where("user_id = ? AND voteable_id = ? AND voteable_type = ?", self.user_id, self.voteable_id, self.voteable_type).count
+    if count == 0
+      true
+    else
+      self.errors.add(:base, "Can not do this")
+      false
+    end
+  end
+
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Vote do
+  it 'only allows one vote for a given user and a given question/answer' do
+    user = User.create!(username: 'ix', password: 'ix')
+    question = user.questions.create!(body: 'soemthing', solved: false)
+    answer = question.answers.create!(:body => "something")
 
+    v = answer.votes.create!(:user_id => user.id, :vote => true)
+
+    expect(v.user).to eq(user)
+    expect(v.voteable_type).to eq('Answer')
+
+    vote2 = answer.votes.new(:user_id => user.id, :vote => false)
+    vote2.save
+    expect(vote2.errors.full_messages).to eq(['Can not do this'])
+  end
 end

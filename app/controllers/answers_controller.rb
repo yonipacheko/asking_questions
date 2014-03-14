f7feaa9b8140c111e2fd72ccbaa58162194bccfb
yoni pacheko <fernando.lbarrajon8@gmail.com>
2014-03-14 16:38:29 +0100

    wip

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -13,18 +13,24 @@ class AnswersController < ApplicationController
     end
   end
 
+
   def vote
     #binding.pry
     @question = Question.find(params[:question_id])
     @questions = Question.all
-    answer = Answer.where(id: params[:id]).first
-    vote = Vote.new(voteable: answer, user: current_user, vote: params[:vote] )
+    @answer = Answer.where(id: params[:id]).first
+    vote = Vote.new(voteable: @answer, user: current_user, vote: params[:vote] )
     if vote.save
       flash[:notice] = 'Your vote was counted'
       redirect_to question_path( @question )
     else
-      flash[:alert] = "You can't vote more than one time"
-      redirect_to question_path ( @question )
+      #flash[:alert] = "You can't vote more than one time"
+      #flash.now[:error]
+        #redirect_to question_path ( @question )
+        #@answer
+        #@question
+      render 'questions/show'
+
     end
     #if answer.votes.find_by(user: current_user)
     #  flash[:alert] = 'A user can vote 1 time!'
